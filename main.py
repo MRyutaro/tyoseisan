1cd326d5f51237bfc16a508e1299eb48e743f4e4
MRyutaro <m2002r1028@icloud.com>
2022-07-07 09:07:47 +0900

    微修正

--- main.py
+++ main.py
@@ -7,40 +7,40 @@ def check_dates(persons):
     all_names = setup_name(all_data)
     all_dates = setup_date(all_data)
     all_true_flase = setup_true_false(all_data)
-    
+
     check_name_exist = [0] * len(all_names)
     for i in range(len(all_names)):
-      for j in range(len(persons)):
-        if persons[j] == all_names[i]:
-          check_name_exist[i] = 1
-    
+        for j in range(len(persons)):
+            if persons[j] == all_names[i]:
+                check_name_exist[i] = 1
+
     print("======================================================\n")
 
     for name in range(len(all_names)):
-      if check_name_exist[name] == 1:
-        print(all_names[name])
+        if check_name_exist[name] == 1:
+            print(all_names[name])
 
     persons_count = 0
     for name in range(len(check_name_exist)):
-      if check_name_exist[name] == 1:
-        persons_count += 1
+        if check_name_exist[name] == 1:
+            persons_count += 1
     print(f"の{persons_count}人の予定を確認します。\n可能な日は、\n")
 
     possible_all_dates = list()
     for date in range(len(all_dates)):
-      possible_count = 0
-      for name in range(len(all_names)):
-        if check_name_exist[name] == 1:
-          if all_true_flase[date][name] == '○':
-            possible_count += 1
-      if possible_count == persons_count:
-        possible_all_dates.append(all_dates[date])
+        possible_count = 0
+        for name in range(len(all_names)):
+            if check_name_exist[name] == 1:
+                if all_true_flase[date][name] == '○':
+                    possible_count += 1
+        if possible_count == persons_count:
+            possible_all_dates.append(all_dates[date])
 
     for date in range(len(possible_all_dates)):
-      print(f"{possible_all_dates[date]}")
+        print(f"{possible_all_dates[date]}")
     print("\nです。\n")
     print("======================================================\n")
-          
+
 
 def setup_name(all_data):
     all_names = list()
@@ -59,10 +59,10 @@ def setup_date(all_data):
 def setup_true_false(all_data):
     all_true_false = list()
     for i in range(3, len(all_data)-1):
-      tmp_all_true_false = list()
-      for j in range(1, len(all_data[i])):
-          tmp_all_true_false.append(all_data[i][j])
-      all_true_false.append(tmp_all_true_false)
+        tmp_all_true_false = list()
+        for j in range(1, len(all_data[i])):
+            tmp_all_true_false.append(all_data[i][j])
+        all_true_false.append(tmp_all_true_false)
     return all_true_false
 
 
@@ -76,18 +76,19 @@ def read_csv_file():
             all_data.append(row)
     return all_data
 
+
 def input_names():
-  flag = True
-  persons = list()
-  while(flag):
-    input_str = input("確認したい人の名前を漢字名字で書いて。記入をやめるときは「やめる」と書いて。")
-    if input_str == "やめる":
-      print("\n")
-      flag = False
-    else:
-      persons.append(input_str)
-  return persons
+    flag = True
+    persons = list()
+    while(flag):
+        input_str = input("確認したい人の名前を漢字名字で書いて。記入をやめるときは「やめる」と書いて。")
+        if input_str == "やめる":
+            print("\n")
+            flag = False
+        else:
+            persons.append(input_str)
+    return persons
+
 
 if __name__ == "__main__":
-    possible_dates = check_dates(input_names())
-    
+    check_dates(input_names())
